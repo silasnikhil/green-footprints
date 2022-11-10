ab75e392c208e6eddd6fd2e9a49aadf1ff7c060a
shreya361999 <kalalshreya1999@gmail.com>
2022-11-10 17:35:47 +0530

    usecase done

--- src/pages/Usecase.jsx
+++ src/pages/Usecase.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import bg1 from '../assets/bg1.png'
+import "./Usecase.css";
+
 
 
 const Usecase = () => {
   return (
-    <div className="backgroundimg">
-      <img src = {bg1} alt="bg1" />
+    <div className="usecase h-screen w-screen " >
+      {/* <div >
+        <h1 className="gradient__text">Green Footprints</h1>
+      <p className="text-indigo-900">This system helps the companies move towards to the greener cloud, 
+        by calculating the carbon emissions from differnt cloud platform </p>
+        </div> */}
+
+
+        <div className="header section__padding" id="home">
+      <div className="header-content text-indigo-900">
+        <h1 className="gradient__text">Green Footprints</h1>
+        <p className="text-indigo-900">
+        This system helps the companies move towards to the greener cloud, 
+        by calculating and comparing the carbon emissions from differnt cloud platform.
+        </p>
+      </div>
+
+        
+      </div>
+      </div>
+          
+      
+          
+      
      
-    {/* <h1 style={{color: "red"}}>Hello</h1> */}
-    </div>
+    
+  
   )
 }
 
