4d62ae24aea9a95f565b7da79be16808547c5ef9
shreya361999 <kalalshreya1999@gmail.com>
2022-11-10 11:28:02 +0530

    Use cases initiate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
+import Usecase from "./pages/Usecase";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/usecase" element={<Usecase />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Usecase.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Usecase = () => {
+  return (
+    <div className="backgroungimg">
+     
+      
+    <h1 style={{color: "red"}}>Hello</h1>
+    </div>
+  )
+}
+
+export default Usecase
