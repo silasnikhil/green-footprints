81597fab3508c51149505fbd89e1091c8ff2fb4e
shreya361999 <kalalshreya1999@gmail.com>
2022-11-11 11:37:38 +0530

    Usecase final

--- src/pages/Usecase.jsx
+++ src/pages/Usecase.jsx
@@ -4,19 +4,16 @@ import "./Usecase.css";
 
 
 
+
 const Usecase = () => {
   return (
     <div className="usecase h-screen w-screen " >
-      {/* <div >
-        <h1 className="gradient__text">Green Footprints</h1>
-      <p className="text-indigo-900">This system helps the companies move towards to the greener cloud, 
-        by calculating the carbon emissions from differnt cloud platform </p>
-        </div> */}
+      
 
 
-        <div className="header section__padding" id="home">
-      <div className="header-content text-indigo-900">
-        <h1 className="gradient__text">Green Footprints</h1>
+        <div className="section__padding" >
+      <div className="usecase-content text-indigo-900">
+        <h1 className="gradient__text" >Green Footprints</h1>
         <p className="text-indigo-900">
         This system helps the companies move towards to the greener cloud, 
         by calculating and comparing the carbon emissions from differnt cloud platform.
